c59d5ba8aba826e61ab6f2dca5416f276fd79c70
Dharmie Folorunso <dharmelorlahfolorunso@gmail.com>
2025-08-20 19:28:09 +0100

    Created Echo-Music-Player app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-[Manrope] text-7xl'>Hello</h1>
+      <h1 className='font-SF-Pro-Display text-base'>Home</h1>
+    </div>
+  )
+}
+
+export default App
